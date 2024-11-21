882a59df6616c4c4383c789c959f9b281821afcd
sandeep2351 <sandeepkalyan299@gmail.com>
2024-11-21 15:21:47 +0530

    dateutils

--- frontend/src/components/ExperienceSection.jsx
+++ frontend/src/components/ExperienceSection.jsx
@@ -1,6 +1,6 @@
 import { Briefcase, X } from "lucide-react";
 import { useState } from "react";
-import { formatDate } from "../utils/dateUtils";
+import { formatDate } from "../Utils/dateUtils";
 
 const ExperienceSection = ({ userData, isOwnProfile, onSave }) => {
 	const [isEditing, setIsEditing] = useState(false);
